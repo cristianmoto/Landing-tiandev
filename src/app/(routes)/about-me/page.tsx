913d26b4d53c1a4d4d9edde10f778211e09d0afe
Cristian Aguirre <cristianmoto066@gmail.com>
2025-03-26 16:00:57 -0300

    about-me

---
+++ src/app/(routes)/about-me/page.tsx
@@ -0,0 +1,13 @@
+import Container from "@/app/components/Container"
+import TransitionsPage from "@/app/components/TransitionsPage"
+
+const PageAboutMe = () => {
+  return (
+   <>
+   <TransitionsPage/>
+   <Container>From page</Container>
+   </>
+  )
+}
+
+export default PageAboutMe
